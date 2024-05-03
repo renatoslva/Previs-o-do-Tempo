92689de5050bb9bbc6b60c60021a37e66330c828
RENATODEVPROENÇA <101299005+renatoslva@users.noreply.github.com>
2024-05-03 19:40:33 -0300

    FiM

--- projeto-clima/script.js
+++ projeto-clima/script.js
@@ -1,11 +1,23 @@
 const key = "e65da520adb62717911abdbe5fd3f5fd"
 
+function dataonscreen(data){
+    console.log(data)
+    document.querySelector(".city").innerHTML = "Tempo em " + data.name
+    document.querySelector(".temperature").innerHTML = Math.floor (data.main.temp) + " °c"
+    document.querySelector(".prediction-text").innerHTML = data.weather[0].description
+    document.querySelector(".moisture").innerHTML = "Umidade do Ar " + data.main.humidity + "%"
+    document.querySelector(".forecast-image").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+}
+
 
  async function searchcity (input){
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${key}&lang=pt_br `).then(response => response.json())
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${key}&lang=pt_br&units=metric `).then(response => response.json())
 
-       console.log (dados)
-}
+    dataonscreen(data)
+    
+        
+    }
+
 
 function Clickonthebutton(){
     const input = document.querySelector(".input-city").value
@@ -14,3 +26,4 @@ function Clickonthebutton(){
 
    
 }
+//https://openweathermap.org/img/wn/04n.png
